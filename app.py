2ac1fb2c260f8d2e29220b6db1583e8dd7b10746
Hritik Ranjan <hritikranjan045@gmail.com>
2023-10-17 18:24:11 +0530

    improvised NetworkNodes

--- app.py
+++ app.py
@@ -2,22 +2,17 @@ from flask import Flask, jsonify, render_template, request
 import requests
 import sys
 from node import *
+from util import CLIENT as client
 
 # Initialise the port and app
 app = Flask(__name__)
 port = sys.argv[1]
 selfAddress = f'http://localhost:{port}'
-maintainNetworkNodes(selfAddress)
-print("D--",distinctNodes,"  Nodes--",networkNodes,"  active--",activeNodes)
-
-nodes = []
 
 # Create route for the home page
 @app.route('/', methods = ['GET'])
 def home():
-    data ={"distinctNodes":distinctNodes,
-           "networkNodes":networkNodes,
-           "activeNodes":activeNodes}
+    data =networkNodes
     return render_template('home.html', data=data)
 
 # Chose the node where you want to connect
@@ -32,53 +27,36 @@ def displayConnectPage():
 def connect():
     port = request.form.get('port', type=int)
     newNodeUrl = f'http://localhost:{port}'
-    maintainNetworkNodes(newNodeUrl)
-    print("D--",distinctNodes,"  Nodes--",networkNodes,"  active--",activeNodes)
-    data = {
-        "newNodeUrl": newNodeUrl
-    }
+    response = requests.get(newNodeUrl)
+    if newNodeUrl!=selfAddress:
+        data = {
+            "newNodeUrl": newNodeUrl
+        }
+        for node in networkNodes:
+            address = f'{node}/connect/register'
+            response = requests.post(address, json=data)
+            if response.status_code == 200:
+                print(f'Connected Successfully with node {node}')
 
-    if newNodeUrl not in nodes and newNodeUrl != selfAddress:
-        nodes.append(data['newNodeUrl'])
-    # else:
-    #     print(f'{node} is already added or this is address of self node.')
-
-    # Add this node to all the nodes in the network
-    for node in nodes:
-        address = f'{node}/connect/register'
-        response = requests.post(address, json=data)
+        # Now add all the existing nodes to the new node
+        bulkRegisterUrl = f'{newNodeUrl}/connect/bulk-register'
+        data = {
+            "nodes": list(networkNodes) + [selfAddress]
+        }
+        response = requests.post(bulkRegisterUrl, json=data)
         if response.status_code == 200:
-            print(f'Connected Successfully with node {node}')
-        else:
-            print(f"Could not register to node {node}")
-   
-
-    # Now add all the existing nodes to the new node
-    bulkRegisterUrl = f'{newNodeUrl}/connect/bulk-register'
-    data = {
-        "nodes": nodes + [selfAddress]
-    }
-    response = requests.post(bulkRegisterUrl, json=data)
-    if response.status_code == 200:
-        print("Successfully added a new node")
-    return render_template('port.html', port=port, nodes=nodes)
-
-@app.route('/connect/connect2', methods=['POST', 'GET'])
-def connect2():
-    pass
-
+            print("Successfully added a new node")
+        maintainNetworkNodes(newNodeUrl)
+        print(f"selfAddress:{selfAddress} , networkNodes: {networkNodes}")
+    return render_template('port.html', port=port, nodes=networkNodes)
 
 # Add one node to the current node as a neighbour
 @app.route('/connect/register',  methods = ["GET", "POST"])
 def register():
     data = request.json
     print(data)
-
-    if data['newNodeUrl'] not in nodes and data['newNodeUrl'] != selfAddress:
-        nodes.append(data['newNodeUrl'])
-    else:
-        print('This node is already added or this is address of self node.')
-    print(nodes)
+    maintainNetworkNodes(data['newNodeUrl'])
+    print(f"selfAddress:{selfAddress} , networkNodes: {networkNodes}")
 
     data = {
         'status': "SUCCESS",
@@ -89,33 +67,39 @@ def register():
 # Return status ACTIVE if the node is running
 @app.route('/activeStatus', methods=['GET'])
 def activeStatus():
+    maintainActiveNodes()
+    print(activeNodes)
     data = {
         'status': 'ACTIVE',
-        'address': selfAddress
+        'selfAddress': selfAddress,
+        'activeNodes':activeNodes
     }
     return jsonify(data)
-
 
 # Add all the nodes in the network returned by the initial node we connected to
 @app.route('/connect/bulk-register', methods = ['GET', 'POST'])
 def bulkRegister():
     data = request.json
     for node in data['nodes']:
-        if node not in nodes and node != selfAddress:
-            nodes.append(node)
-        else:
-            print(f'{node} is already added or this is address of self node.')
-    
+        maintainNetworkNodes(node)
+    print(f"selfAddress:{selfAddress} , networkNodes: {networkNodes}")
     return {
-        'status': "SUCCESS",
-        'nodes': nodes
+        "status": "SUCCESS"
     }
 
 @app.route('/doctor/register', methods = ['GET', 'POST'])
 def registerDoctor():
     return render_template('doctor_register.html')
 
+@app.route('/process_form', methods=['POST'])
+def process_form():
+    name = request.form.get('name')
+    email = request.form.get('email')
+
+    # You can now use the 'name' and 'email' variables for further processing.
+    # For example, you can store the data in a database, perform some logic, or render a response page.
+
+    return f"Received data: Name - {name}, Email - {email}"
 
 if __name__ == "__main__":
     app.run(port=port, debug=True)
-    # run_app_on_port(port)
--- node.py
+++ node.py
@@ -1,34 +1,20 @@
 import requests
-distinctNodes=set()
-networkNodes={}
+networkNodes=set()
 activeNodes=set()
 
 def maintainNetworkNodes(nodeAddress):
-    global distinctNodes
-    global nodes
+    global networkNodes
     global activeNodes
-    distinctNodes.add(nodeAddress)
+    networkNodes.add(nodeAddress)
     activeNodes.add(nodeAddress)
-    if nodeAddress not in networkNodes:
-        networkNodes[nodeAddress]=[]
-    print(networkNodes)
-    for i in distinctNodes:
-        if i!=nodeAddress:
-            networkNodes[nodeAddress].append(i)
-    print(networkNodes)
-    for i in networkNodes:
-        if i!=nodeAddress:
-            networkNodes[i].append(nodeAddress)
-    print("3-",networkNodes)
+    print(f" networkNodes: {networkNodes}")
 
 def maintainActiveNodes():
     global activeNodes
-    global distinctNodes
-    for i in distinctNodes:
+    global networkNodes
+    for i in networkNodes:
+        activeNodes.add(i)
+    for i in networkNodes:
         response = requests.get(i)
         if response.status_code != 200:
             activeNodes.discard(i)
-            
-    
-
-
